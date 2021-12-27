367cdae824e730320dc443c91f1635662aa7c778
Tinsus <tinsus@t-online.de>
2021-12-27 23:02:55 +0100

    its done that way :D

--- GW2start.bat
+++ GW2start.bat
@@ -4,4 +4,10 @@ set GW2_path="C:\Program Files\Guild Wars 2"
 set TacO_path="D:\Program Files (x86)\GW2\TacO"
 set BlishHUD_path="D:\Program Files (x86)\GW2\BlishHUD"
 
+
+if exist "GW2start.txt" (
+	del "GW2start.ps1"
+	rename "GW2start.txt" "GW2start.ps1"
+)
+
 powershell.exe -file "GW2start.ps1" '%GW2_path%' '%TacO_path%' '%BlishHUD_path%'
