0d87d6b0fd4b43903d499386e7f547f78759a7ad
Tinsus <tinsus@t-online.de>
2022-01-13 23:36:35 +0100

    prepare for upcoming ini-Stuff

--- GW2start.ps1
+++ GW2start.ps1
@@ -145,6 +145,56 @@ function checkGithub() {
 	}
 }
 
+function Get-IniContent($filePath) {
+    $ini = @{}
+
+    Switch -regex -file $FilePath {
+		# Section
+        "^\[(.+)\]" {
+            $section = $matches[1]
+            $ini[$section] = @{}
+            $CommentCount = 0
+        }
+		# Comment
+        "^(;.*)$" {
+            $value = $matches[1]
+            $CommentCount = $CommentCount + 1
+            $name = "Comment" + $CommentCount
+            $ini[$section][$name] = $value
+        }
+		# Key
+        "(.+?)\s*=(.*)" {
+            $name, $value = $matches[1..2]
+            $ini[$section][$name] = $value
+        }
+    }
+
+    return $ini
+}
+
+function Out-IniFile($InputObject, $FilePath) {
+    $outFile = New-Item -ItemType file -Path $Filepath
+    foreach ($i in $InputObject.keys) {
+        if (!($($InputObject[$i].GetType().Name) -eq "Hashtable")) {
+            #No Sections
+            Add-Content -Path $outFile -Value "$i=$($InputObject[$i])"
+        } else {
+            #Sections
+            Add-Content -Path $outFile -Value "[$i]"
+
+            Foreach ($j in ($InputObject[$i].keys | Sort-Object)) {
+                if ($j -match "^Comment[\d]+") {
+                    Add-Content -Path $outFile -Value "$($InputObject[$i][$j])"
+                } else {
+                    Add-Content -Path $outFile -Value "$j=$($InputObject[$i][$j])"
+                }
+            }
+
+            Add-Content -Path $outFile -Value ""
+        }
+    }
+}
+
 # now the non-dynamic stuff:
 
 # clean up before anything else starts
