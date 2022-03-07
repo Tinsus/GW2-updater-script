809a1558de77872658531bb6a02a641fe0162f5c
Tinsus <tinsus@t-online.de>
2022-03-08 00:15:29 +0100

    some cleanups

--- GW2start.ps1
+++ GW2start.ps1
@@ -605,89 +605,9 @@ if ($false) {
 	$groupPaths.Location = New-Object System.Drawing.Size(($groupTaco.Location.X), ($groupArc.Location.Y + $groupArc.height + 10))
 	$groupModules.Location = New-Object System.Drawing.Size(($groupBlish.Location.X), ($groupArc.Location.Y + $groupArc.height + 10))
 
-	<#
-	$ComboBox = New-Object System.Windows.Forms.ComboBox
-
-	$ComboBox.Width = 300
-
-	$Users = get-aduser -filter * -Properties SamAccountName
-
-	Foreach ($User in $Users)
-
-	{
-
-	$ComboBox.Items.Add($User.SamAccountName);
-
-	}
-
-	$ComboBox.Location  = New-Object System.Drawing.Point(60,10)
-
-	$main_form.Controls.Add($ComboBox)
-
-	$Label2 = New-Object System.Windows.Forms.Label
-
-	$Label2.Text = "Last Password Set:"
-
-	$Label2.Location  = New-Object System.Drawing.Point(0,40)
-
-	$Label2.AutoSize = $true
-
-	$main_form.Controls.Add($Label2)
-
-	$Label3 = New-Object System.Windows.Forms.Label
-
-	$Label3.Text = ""
-
-	$Label3.Location  = New-Object System.Drawing.Point(110,40)
-
-	$Label3.AutoSize = $true
-
-	$main_form.Controls.Add($Label3)
-
-	$Button = New-Object System.Windows.Forms.Button
-
-	$Button.Location = New-Object System.Drawing.Size(400,10)
-
-	$Button.Size = New-Object System.Drawing.Size(120,23)
-
-	$Button.Text = "Check"
-
-	$main_form.Controls.Add($Button)
-
-	$Button.Add_Click(
-
-	{
-
-	$Label3.Text =  [datetime]::FromFileTime((Get-ADUser -identity $ComboBox.selectedItem -Properties pwdLastSet).pwdLastSet).ToString('MM dd yy : hh ss')
-
-	}
-
-	)
-
-	$Label3.Text.Visible = $false
-
-	# or $True if you want to show it
-
-	#>
-
-
-
-
-
-
-
-
-
 
 
 	$main_form.ShowDialog()
-
-	#$shell = New-Object -ComObject Shell.Application
-
-	#$selectedfolder = $shell.BrowseForFolder( 0, 'Select a folder to proceed', 16, $shell.NameSpace( 17 ).Self.Path ).Self.Path
-
-	#$answer = [System.Windows.MessageBox]::Show( "Dou you want to remove this user?", " Removal Confirmation", "YesNoCancel", "Information" )
-
 
 <#
     CheckBox – used to list some options and select them prior to running script;
